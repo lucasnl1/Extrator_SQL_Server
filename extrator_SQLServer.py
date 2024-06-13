e134508702f80e470bafdacc3a886a04ea090700
Lucas nascimento <38538380+majorlukas@users.noreply.github.com>
2024-06-13 15:18:42 -0300

    Update extrator_SQLServer.py

--- extrator_SQLServer.py
+++ extrator_SQLServer.py
@@ -38,7 +38,24 @@ def fetch_data():
     try:
         cur = conn.cursor()
         cur.execute('''
-        SQL a ser executado para consulta
+         SELECT 
+	    LOCALIZACAO, 
+	    DATA,
+	    CLI_NOME,
+	    CLI_CPF,
+	    UF,
+	    CIDADE,
+	    VENDEDOR,
+	    PRODUTO,
+	    QUANTIDADE, 
+	    VALOR_TOTAL
+	FROM VW_FAT_DET
+    JOIN CLIENTE C ON CLI_COD = CD_CLI_GER
+    WHERE FOR_CODI = '01076'
+    AND CONVERT(DATETIME, DATA, 103)>= DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()) - 1, 0)
+    AND CONVERT(DATETIME, DATA, 103) < DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()), 0)
+    AND NATUREZA IN ('VEN')
+    ORDER BY DATA ASC
         ''')
         #retorna os dados recolhidos no select
         rows = cur.fetchall()
